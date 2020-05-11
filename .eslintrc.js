560ab72c29aa33fdd9fa617c5cb30bd55d7d1dd6
Akane Sugiuchi <asugiuchi@tohmatsu.co.jp>
2020-05-11 14:36:27 +0900

    Updated no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    "no-console": "warn",
+    "no-console": "error",
     '@typescript-eslint/no-unused-vars': 'error'
   }
 }
